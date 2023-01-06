56d5dac60d3fc9976dc9af2319e1c3ce7e0ec079
joongwon <winterdawnlight@snu.ac.kr>
2023-01-06 20:26:44 +0900

    add joystick

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image _backgroundImage;
+    private Image _joystickImage;
+    private Vector3 _inputVector;
+
+    private void Start()
+    {
+        _backgroundImage = GetComponent<Image>();
+        _joystickImage = transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // get position in [-.5, .5]
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(_backgroundImage.rectTransform, eventData.position,
+            eventData.pressEventCamera, out var pos);
+        var sizeDelta = _backgroundImage.rectTransform.sizeDelta;
+        pos.x /= sizeDelta.x;
+        pos.y /= sizeDelta.y;
+        
+        // normalize in [-1, 1]
+        _inputVector = new Vector3(pos.x * 2, pos.y * 2, 0);
+        _inputVector = _inputVector.magnitude > 1 ? _inputVector.normalized : _inputVector;
+        
+        // move joystick image
+        _joystickImage.rectTransform.anchoredPosition = new Vector3(_inputVector.x * (sizeDelta.x / 3),
+            _inputVector.y * (sizeDelta.y / 3));
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // reset
+        _inputVector = Vector3.zero;
+        _joystickImage.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // begin drag
+        OnDrag(eventData);
+    }
+    
+    public float GetHorizontalValue()
+    {
+        return _inputVector.x;
+    }
+    
+    public float GetVerticalValue()
+    {
+        return _inputVector.y;
+    }
+}
