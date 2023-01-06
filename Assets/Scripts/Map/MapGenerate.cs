d7333ab09e6dbda431c3fc209e4313601149ac04
ray_notebook <jw1027@kaist.ac.kr>
2023-01-06 22:18:41 +0900

    Cube, Liquid 간단한 제어
    ㅁㄴㅇㄻㄴㅇㄹ

---
+++ Assets/Scripts/Map/MapGenerate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerate : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject box;
+    private GameObject emitter;
+    private GameObject liquidLimit;
+
+
+    // x, y : center align
+    public int Generate(double x, double y, double w, double h) {
+
+        return 0;
+    }
+}
