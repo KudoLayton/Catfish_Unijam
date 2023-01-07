6cb6d8ee6ab2a4dc4e61f0d53e0d5e610438f43a
joongwon <winterdawnlight@snu.ac.kr>
2023-01-07 23:37:27 +0900

    restrict angle in 45

--- Assets/Scripts/GyroPlaneController.cs
+++ Assets/Scripts/GyroPlaneController.cs
@@ -4,7 +4,7 @@ public class GyroPlaneController : MonoBehaviour
 {
     private float _angle;
     private float _prevAngle;
-    private Vector3 _gravity;
+    private Vector3 _faceDirection;
 
     void Start()
     {
@@ -23,14 +23,25 @@ public class GyroPlaneController : MonoBehaviour
 #if UNITY_EDITOR
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
-        _gravity.x += horizontal * Time.deltaTime;
-        _gravity.z += vertical * Time.deltaTime;
-        _gravity = Vector3.ClampMagnitude(_gravity, 1);
-        _gravity.y = Mathf.Sqrt(1 - _gravity.x * _gravity.x - _gravity.z * _gravity.z);
+        _faceDirection.x += horizontal * Time.deltaTime;
+        _faceDirection.z += vertical * Time.deltaTime;
+        _faceDirection = Vector3.ClampMagnitude(_faceDirection, 1);
+        _faceDirection.y = Mathf.Sqrt(1 - _faceDirection.x * _faceDirection.x - _faceDirection.z * _faceDirection.z);
 #else
-        _gravity = new Vector3(Input.gyro.gravity.x, -Input.gyro.gravity.z, Input.gyro.gravity.y);
+        _faceDirection = new Vector3(Input.gyro.gravity.x, -Input.gyro.gravity.z, Input.gyro.gravity.y);
 #endif
-        var planeRotation = Quaternion.FromToRotation(Vector3.up, _gravity);
+        if (_faceDirection.y < 0) return;
+        var planeDirection = new Vector3(_faceDirection.x, _faceDirection.y, 0).normalized;
+        // outside +- 45
+        if (planeDirection.y < planeDirection.x)
+        {
+            planeDirection = new Vector3(1, 1, 0);
+        } 
+        else if (planeDirection.y < -planeDirection.x)
+        {
+            planeDirection = new Vector3(-1, 1, 0);
+        }
+        var planeRotation = Quaternion.FromToRotation(Vector3.up, planeDirection);
         transform.rotation = planeRotation;
     }
 }
