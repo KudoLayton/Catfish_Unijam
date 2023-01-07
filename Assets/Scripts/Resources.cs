b716152b364ab5db2ab9e443af30c304b954a065
ray_notebook <jw1027@kaist.ac.kr>
2023-01-07 11:45:36 +0900

    Editor 작업 없앰
    ㅁㄴㅇㄹ

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject blockPrefab;
+    public GameObject liquidPrefab;
+    public GameObject emitterPrefab;
+}
