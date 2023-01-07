5e7bac4b60fc608ba41a5bb3b097591577300a66
joongwon <winterdawnlight@snu.ac.kr>
2023-01-08 04:35:57 +0900

    add marimo smooth turn

--- Assets/Scripts/FishBehavior.cs
+++ Assets/Scripts/FishBehavior.cs
@@ -6,8 +6,13 @@ public class FishBehavior : MonoBehaviour
 {
     public string color;
     private bool _isExiting;
+    private Vector3 _exitVelocity;
+    private Vector3 _beforeExitVelocity;
+    private float _exitTime;
+    private Rigidbody _rigidbody;
     [SerializeField] private float exitSpeed;
     [SerializeField] private float exitDistance;
+    [SerializeField] private float exitTurnTime;
 
     public void SetColor(string color)
     {
@@ -17,12 +22,18 @@ public class FishBehavior : MonoBehaviour
     private void Start()
     {
         _isExiting = false;
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (_isExiting && Mathf.Abs(transform.position.x) > exitDistance)
-            gameObject.SetActive(false);
+        if (_isExiting)
+        {
+            var progress = (Time.time - _exitTime) / exitTurnTime;
+            _rigidbody.velocity = progress > 1 ? _exitVelocity : Vector3.Lerp(_beforeExitVelocity, _exitVelocity, progress);
+            if (Mathf.Abs(transform.position.x) > exitDistance)
+                gameObject.SetActive(false);
+        }
     }
 
     public void EnterJellyfish()
@@ -37,14 +48,15 @@ public class FishBehavior : MonoBehaviour
 
     public void EatMarimo()
     {
-        var rigidBody = GetComponent<Rigidbody>();
-        rigidBody.isKinematic = false;
-        rigidBody.useGravity = false;
+        _rigidbody.isKinematic = false;
+        _rigidbody.useGravity = false;
         GetComponent<Collider>().enabled = false;
         if (transform.position.x < 0)
-            rigidBody.velocity = new Vector3(-exitSpeed, 0, 0);
+            _exitVelocity = new Vector3(-exitSpeed, 0, 0);
         else
-            rigidBody.velocity = new Vector3(exitSpeed, 0, 0);
+            _exitVelocity = new Vector3(exitSpeed, 0, 0);
+        _beforeExitVelocity = _rigidbody.velocity;
+        _exitTime = Time.time;
         _isExiting = true;
     }
 }
