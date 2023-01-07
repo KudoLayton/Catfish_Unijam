3afe04f8d4ffbd81f5cdf4e74246b7808dcbdb18
ray_notebook <jw1027@kaist.ac.kr>
2023-01-08 07:40:08 +0900

    빨간 무언가가 없는 버전
    무야호~

--- Assets/Scripts/FishRotator.cs
+++ Assets/Scripts/FishRotator.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class FishRotator : MonoBehaviour
 {
     private Vector3 InitialForward = Vector3.forward;
-    private Rigidbody rigidbody;
-    [SerializeField] private GameObject plane;
+    private Rigidbody _rigidbody;
+    private GameObject plane;
     void Start()
     {
-        rigidbody = transform.parent.GetComponent<Rigidbody>();
+        _rigidbody = transform.parent.GetComponent<Rigidbody>();
+        plane = GameObject.FindWithTag("Plane");
     }
 
     // Update is called once per frame
@@ -17,7 +18,7 @@ public class FishRotator : MonoBehaviour
     {
         Debug.Assert(plane);
         Vector3 planeNormal = plane.transform.localRotation * Vector3.up;
-        Vector3 lookFor = Vector3.ProjectOnPlane(rigidbody.velocity, planeNormal);
+        Vector3 lookFor = Vector3.ProjectOnPlane(_rigidbody.velocity, planeNormal);
         lookFor = Vector3.Normalize(lookFor);
         transform.rotation = Quaternion.LookRotation(lookFor, planeNormal);
     }
