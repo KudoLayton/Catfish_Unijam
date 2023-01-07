a7f5ace9a8de13c7080b71801fa4f7cac0919c5a
joongwon <winterdawnlight@snu.ac.kr>
2023-01-08 06:24:56 +0900

    add marimo regen

--- Assets/Scripts/MarimoController.cs
+++ Assets/Scripts/MarimoController.cs
@@ -1,16 +1,18 @@
+using System.Collections;
 using UnityEngine;
 
 public class MarimoController : MonoBehaviour
 {
+    [SerializeField] private MarimoGenerator generator;
     private void OnTriggerEnter(Collider other)
     {
         var fishBehavior = other.GetComponent<FishBehavior>();
         if (fishBehavior)
         {
-            Debug.Log("Fish entered marimo: " + fishBehavior.name);
             GetComponent<Collider>().enabled = false;
             other.enabled = false;
             fishBehavior.EatMarimo();
+            generator.SpawnMarimo();
             gameObject.SetActive(false);
         }
     }
