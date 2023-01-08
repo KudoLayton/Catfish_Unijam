588c09470dd67e24095dd4f990be3a985d54a38d
joongwon <winterdawnlight@snu.ac.kr>
2023-01-08 10:23:18 +0900

    add popup

--- Assets/Scripts/CatPopupManager.cs
+++ Assets/Scripts/CatPopupManager.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CatPopupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sprite[] catSprites;
+    [SerializeField] private GameObject catPopupPrefab;
+    [SerializeField] private float popupDuration;
+    private const float Epsilon = 0.00001f;
+
+    private Sprite GetRandomImage()
     {
-        
+        return catSprites[Mathf.FloorToInt((Random.value - Epsilon) * catSprites.Length)];
+    }
+    
+    public void ShowCatPopup(GameObject catObject)
+    {
+        var popup = Instantiate(catPopupPrefab, catObject.transform);
+        var behavior = popup.GetComponent<CatPopupBehavior>();
+        behavior.SetDuration(popupDuration);
+        popup.GetComponent<Renderer>().material.mainTexture = GetRandomImage().texture;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        // Test();
+    }
+
+    private void Test()
+    {
+        var catObjects = GameObject.FindGameObjectsWithTag("Cat");
+        foreach (var catObject in catObjects)
+        {
+            ShowCatPopup(catObject);
+        }
     }
 }
