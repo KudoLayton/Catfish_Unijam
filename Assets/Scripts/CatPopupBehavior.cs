84d3f6c2542d22a71a2d801f289c05008a0b3013
joongwon <winterdawnlight@snu.ac.kr>
2023-01-08 10:23:18 +0900

    add popup

---
+++ Assets/Scripts/CatPopupBehavior.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class CatPopupBehavior : MonoBehaviour
+{
+    private float _startTime;
+    private float _duration;
+
+    private void Start()
+    {
+        _startTime = Time.time;
+    }
+
+    public void SetDuration(float duration)
+    {
+        _duration = duration;
+    }
+
+    void Update()
+    {
+        var progress = (Time.time - _startTime) / _duration;
+        if (progress < 0.3f)
+        {
+            transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, progress / 0.3f);
+        }
+        else if (progress < 0.7f)
+        {
+            transform.localScale = Vector3.one;
+        } 
+        else if (progress < 1)
+        {
+            // become transparent
+            var color = GetComponent<Renderer>().material.color;
+            color.a = 1 - (progress - 0.7f) / 0.3f;
+            GetComponent<Renderer>().material.color = color;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
